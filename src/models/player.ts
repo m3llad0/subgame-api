72fdcd7067ba805122cb3d12db98b6d1826a6b5b
m3llad0 <a01655451@tec.mx>
2023-07-05 23:52:46 -0600

    created player class

---
+++ src/models/player.ts
@@ -0,0 +1,33 @@
+import { Module } from 'module';
+import {Model} from 'sequelize';
+
+interface PlayerAttributes {
+    id: number,
+    username: string,
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+    class Player extends Model<PlayerAttributes> implements PlayerAttributes {
+        id!: number;
+        username!: string;
+        static associate(models: any){
+            Player.belongsToMany(models.Achievement, {
+                through: "PlayerAchievements"
+            });
+        };
+    }
+
+    Player.init({
+        id: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            primaryKey: true,
+        },
+        username: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: "Player"
+    });
+
+    return Player;
+};
