d990ed99118e802b87c10f31b74701e6fa4b5f61
m3llad0 <a01655451@tec.mx>
2023-07-06 19:09:54 -0600

    added conection to db

--- src/providers/server.ts
+++ src/providers/server.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response } from "express";
+import db from "../models";
 
 class Server{
     private app : express.Application;
@@ -31,8 +32,22 @@ class Server{
         })
     }
 
+    private async connectDB(){
+        try{
+
+            await db.sequelize.sync();
+            console.log('Connected to database');
+        }catch(err){
+
+            console.log(err);
+        }
+    }
+
     public async init(){
         
+        /*Connect to database*/
+        await this.connectDB();
+
         this.app.listen(this.port, () => {
             console.log(`Server:: Running @'http://localhost:${this.port}'`)
         })
