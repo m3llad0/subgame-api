c04d804b04fcc71155bd43cc4b4e2741ed2bf43f
m3llad0 <a01655451@tec.mx>
2023-07-08 19:03:05 -0600

    added export for db test

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,3 +4,7 @@ export const DB_NAME: string = process.env.DB_NAME ? process.env.DB_NAME: 'devel
 export const DB_USER: string = process.env.DB_USER ? process.env.DB_USER: 'root';
 export const DB_PASSWORD: string = process.env.DB_PASSWORD ? process.env.DB_PASSWORD: 'password';
 export const DB_HOST: string = process.env.DB_HOST ? process.env.DB_HOST: 'localhost';
+export const DB_NAME_TEST: string = process.env.DB_NAME ? process.env.DB_NAME: 'tes'
+export const DB_USER_TEST: string = process.env.DB_USER ? process.env.DB_USER: 'root';
+export const DB_PASSWORD_TEST: string = process.env.DB_PASSWORD ? process.env.DB_PASSWORD: 'password';
+export const DB_HOST_TEST: string = process.env.DB_HOST ? process.env.DB_HOST: 'localhost';
