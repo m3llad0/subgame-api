95deb19ec8f488aed46bfddda863bb368d8f9967
m3llad0 <a01655451@tec.mx>
2023-07-12 19:14:05 -0600

    add production environment

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import Server from './providers/server';
 import express from 'express';
 import cors  from 'cors';
+import { NODE_ENV } from './config';
 import PlayerController from './controllers/userController';
 import AchievementController from './controllers/achievementController';
 
 
 const app = new Server({
     port: 8080,
-    env: "development",
+    env: NODE_ENV,
     middlewares: [
         express.json(),
         express.urlencoded({extended: true}),
